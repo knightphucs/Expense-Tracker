2dea2a0ee8fe7e22fbcecae0ca66ac2d6a5d4ad7
Phuc Gia <phuchoang3103@gmail.com>
2024-04-11 16:37:18 +0700

    Done Transaction task

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,6 +6,7 @@ namespace ExpenseTracker.Models {
         [Key]
         public int CategoryId { get; set; }
         [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required.")]
         public string? Title { get; set; }
         [Column(TypeName = "nvarchar(5)")]
         public string? Icon { get; set; }
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -5,13 +5,30 @@ namespace ExpenseTracker.Models {
     public class Transaction {
         [Key]
         public int TransactionId { get; set; }
-
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
         //CategoryId
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")] 
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon 
+        {
+            get 
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount {
+            get {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
     }
 }
